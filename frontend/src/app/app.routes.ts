40e8bd6abc5506df7f0fc8ddd518def693a0a9ca
gheb_modu <modugno.gabriele@iisgalvanimi.edu.it>
2026-05-23 16:52:35 +0000

    struttura frontend

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { CategorieComponent } from './components/categorie/categorie.component';
+import { RistorantiComponent } from './components/ristoranti/ristoranti.component';
+import { DettaglioRistoranteComponent } from './components/ristoranti/dettaglio-ristorante.component';
+import { DettaglioPiattoComponent } from './components/piatti/dettaglio-piatto.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '',                              redirectTo: 'categorie', pathMatch: 'full' },
+  { path: 'categorie',                     component: CategorieComponent },
+  { path: 'ristoranti',                    component: RistorantiComponent },
+  { path: 'ristoranti/categoria/:id',      component: RistorantiComponent },
+  { path: 'ristoranti/:id',               component: DettaglioRistoranteComponent },
+  { path: 'piatti/:id',                    component: DettaglioPiattoComponent },
+  { path: '**',                            redirectTo: 'categorie' }
+];
